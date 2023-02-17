9ee748feaa67fdf0ac8dcec759a4c5dde549ac03
Lilia Bekuzina <lilia.bekuzina1@gmail.com>
2023-02-17 20:37:54 +0100

    create BankServiceImplTest

---
+++ src/test/java/com/bank/service/impl/BankServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.bank.service.impl;
+
+import junit.framework.TestCase;
+
+public class BankServiceImplTest extends TestCase {
+
+    public void testWithdraw() {
+    }
+
+    public void testDeposit() {
+    }
+}
