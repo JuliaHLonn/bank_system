39b2dcd0d09349e02f4d4580a2947d1fdf63d937
Julia H. Lonn <julia.halvdansson.lonn@iths.se>
2023-02-17 12:57:50 +0100

    test deposit

--- bank-system/src/test/java/com/bank/service/impl/BankServiceImplTest.java
+++ bank-system/src/test/java/com/bank/service/impl/BankServiceImplTest.java
@@ -9,26 +9,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BankServiceImplTest {
-  private BankAccount bankAccount;
-  private BankServiceImpl bankServiceImpl;
+    private BankAccount bankAccount;
+    private BankServiceImpl bankServiceImpl;
 
-  @BeforeAll
-    public void setup(){
-      bankAccount = new BankAccount("Johan","32");
-      bankServiceImpl = new BankServiceImpl();
+    @BeforeAll
+    public void setup() {
+        bankAccount = new BankAccount("Johan", "32");
+        bankServiceImpl = new BankServiceImpl();
 
-  }
-@Test
-  public void withdrawShouldWorkWithAmountLessThenBalance(){
-    bankServiceImpl.deposit(bankAccount,380);
-    bankServiceImpl.withdraw(bankAccount,200);
-    assertEquals(180,bankAccount.getBalance());
-  }
 
-  @Test
-  public void withdrawShouldFailWithAmountBiggerThenBalance(){
-    bankServiceImpl.deposit(bankAccount,380);
-    bankServiceImpl.withdraw(bankAccount,400);
-    assertEquals(380,bankAccount.getBalance());
-  }
+    }
+
+    @Test
+    public void withdrawShouldWorkWithAmountLessThenBalance() {
+        bankAccount.setBalance(0);
+        bankServiceImpl.deposit(bankAccount, 380);
+        bankServiceImpl.withdraw(bankAccount, 200);
+        assertEquals(180, bankAccount.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldFailWithAmountBiggerThenBalance() {
+        bankAccount.setBalance(0);
+        bankServiceImpl.deposit(bankAccount, 380);
+        bankServiceImpl.withdraw(bankAccount, 400);
+        assertEquals(380, bankAccount.getBalance());
+    }
+
+    @Test
+    public void depositShouldWork() {
+        bankAccount.setBalance(0);
+        bankServiceImpl.deposit(bankAccount,120);
+        assertEquals(120,bankAccount.getBalance());
+    }
+
+    @Test
+    public void depositShouldFailWithAmountLessThenZero(){
+        bankAccount.setBalance(100);
+        bankServiceImpl.deposit(bankAccount,-100);
+        assertEquals(100,bankAccount.getBalance());
+    }
+
 }
