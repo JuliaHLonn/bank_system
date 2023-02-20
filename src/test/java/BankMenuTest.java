028e289c1dd98a9579e92a2b357913c5a1e050db
Dmitriy Goncharov <dmitriy.goncharov@iths.se>
2023-02-20 17:00:52 +0100

    Added a test to verify a method call

--- src/test/java/BankMenuTest.java
+++ src/test/java/BankMenuTest.java
@@ -1,5 +1,6 @@
 import com.bank.BankAccount;
 import com.bank.BankMenu;
+import com.bank.service.BankService;
 import com.bank.service.impl.BankServiceImpl;
 import org.junit.jupiter.api.Test;
 
@@ -7,18 +8,31 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class BankMenuTest {
 
 
     String name = "Kalle";
     String customerId = "10";
+    String inputString = "c\n100\nc\nc";
+
+    BankMenu mockBankMenu = mock(BankMenu.class);
+
+
+    @Test
+    public void shouldCallMenu() {
+
+        mockBankMenu.menu();
+        verify(mockBankMenu, times(1)).menu();
+
+    }
 
 
     @Test
     public void shouldNotThrowExceptionToConsole() {
         InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("c\n100\nc\nc".getBytes()));
+        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
 
 
         assertThrows(RuntimeException.class, () -> {
