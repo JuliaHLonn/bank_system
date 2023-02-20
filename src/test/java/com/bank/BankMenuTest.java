dc4cecf0ec83aff09fbd2679d2e657ee39121daa
Selim Köse <selim-kose@live.com>
2023-02-20 16:34:20 +0100

    cleaned upp testfolder

---
+++ src/test/java/com/bank/BankMenuTest.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankMenuTest {
+
+    @Test
+    void testTest(){
+        assertTrue(true);
+    }
+
+}
