918d019be44f1f7781b9a608aa9e4ee4a6ba21a2
Lilia Bekuzina <lilia.bekuzina1@gmail.com>
2023-02-21 11:05:22 +0100

    add setUp and three tests by Lilia

--- src/test/java/BankMenuTest.java
+++ src/test/java/BankMenuTest.java
@@ -2,7 +2,9 @@ import com.bank.BankAccount;
 import com.bank.BankMenu;
 import com.bank.service.BankService;
 import com.bank.service.impl.BankServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -16,16 +18,17 @@ public class BankMenuTest {
     String name = "Kalle";
     String customerId = "10";
     String inputString = "c\n100\nc\nc";
+    BankService bankService;
+    BankMenu bankMenu;
+    BankAccount bankAccount;
 
-    BankMenu mockBankMenu = mock(BankMenu.class);
 
+    @BeforeEach
+    public void setUp() {
 
-    @Test
-    public void shouldCallMenu() {
-
-        mockBankMenu.menu();
-        verify(mockBankMenu, times(1)).menu();
-
+        bankService = Mockito.mock(BankService.class);
+        bankAccount = new BankAccount("Alice", "2");
+        bankMenu = new BankMenu(bankService, bankAccount);
     }
 
 
@@ -39,5 +42,35 @@ public class BankMenuTest {
                     new BankMenu(new BankServiceImpl(), new BankAccount(name, customerId), stdin).menu();
                 }
         );
+    }
+    
+    @Test
+    public void verifiesThatDepositWasCalledExpectedNumberOfTimes() {
+
+        bankAccount.setBalance(100);
+
+        bankService.deposit(bankAccount, 100);
+
+        Mockito.verify(bankService, Mockito.times(1)).deposit(bankAccount, 100);
+    }
+
+    @Test
+    public void verifiesThatWithdrawWasCalledExpectedNumberOfTimes() {
+
+        bankAccount.setBalance(100);
+
+        bankService.withdraw(bankAccount, 100);
+
+        Mockito.verify(bankService, Mockito.times(1)).withdraw(bankAccount, 100);
+    }
+
+    @Test
+    public void verifiesThatMenuWasCalledExpectedNumberOfTimes() {
+        bankMenu = mock(BankMenu.class);
+
+        bankMenu.menu();
+
+        Mockito.verify(bankMenu, Mockito.times(1)).menu();
+
     }
 }
