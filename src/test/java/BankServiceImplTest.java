6cb309868ed5abe29319080fa2e9d059c4a51162
Julia H. Lonn <julia.halvdansson.lonn@iths.se>
2023-02-21 11:55:04 +0100

    Added tests for deposit

--- src/test/java/BankServiceImplTest.java
+++ src/test/java/BankServiceImplTest.java
@@ -1,6 +1,7 @@
 import com.bank.BankAccount;
 import com.bank.service.impl.BankServiceImpl;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -9,12 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankServiceImplTest {
     private BankAccount bankAccount;
-    BankServiceImpl bankServiceImpl = new BankServiceImpl();
+    BankServiceImpl bankServiceImpl;
 
 
-    @BeforeAll
+    @BeforeEach
     public void initialize() {
         bankAccount = new BankAccount("Charles", "1");
+        bankServiceImpl = new BankServiceImpl();
         bankAccount.setBalance(100);
 
     }
@@ -30,17 +32,17 @@ public class BankServiceImplTest {
         bankServiceImpl.withdraw(bankAccount, 180);
         assertEquals(100, bankAccount.getBalance());
     }
+    
+@Test
+public void depositShouldWork() {
+    bankServiceImpl.deposit(bankAccount,120);
+    assertEquals(220,bankAccount.getBalance());
+}
 
     @Test
-    public void testDepositing80ShouldGive180() {
-        bankServiceImpl.deposit(bankAccount, 80);
-        assertEquals(180, bankAccount.getBalance());
-    }
-
-    @Test
-    public void testDepositingNegativeAmountShouldFail(){
+    public void depositShouldFailWithAmountLessThenZero(){
         bankServiceImpl.deposit(bankAccount,-100);
-        assertEquals(100, bankAccount.getBalance());
+        assertEquals(100,bankAccount.getBalance());
     }
 
 
